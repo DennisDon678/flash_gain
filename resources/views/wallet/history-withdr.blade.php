0dd046c3fc0e1c4991c959fd6f29e35f9006254b
Dennis <Dennisdon678@mail.com>
2023-12-23 17:51:58 -0500

    modified

--- resources/views/wallet/history-withdr.blade.php
+++ resources/views/wallet/history-withdr.blade.php
@@ -4,54 +4,40 @@
 <body>
     <div class="back">
         History
-    </div>   
+    </div>
     <div class="b-b"><a href="my_wallet"><i class='bx bx-share'></i></a></div>
 
     <div class="top-rech">
-       <a href="/history-recha"><button>Recharge</button></a>
-       <a href="/history-withdr"> <button>Withdrawal</button></a>
+        <a href="/history-recha"><button>Recharge</button></a>
+        <a href="/history-withdr"> <button>Withdrawal</button></a>
     </div>
-<div class="tot">
-    <div class="tbal">
-        <h2>Total Withderawal</h2>
-        <p>&#8358;20,000</p>
+    @php
+    $hists = App\Models\Withdrawal::where('user','=',Auth::user()->id)->get();
+    @endphp
+    <div class="tot">
+        <div class="tbal">
+            <h2>Total Withderawal</h2>
+            <p>&#8358;20,000</p>
+        </div>
     </div>
-</div>
+    @if(count($hists)>0)
+    @foreach($hists as $hist)
     <div class="withd">
         <div class="withd-list">
             <div class="w-list1">
                 <p>Flash-Ord</p>
-                <span>&#8358;71,000</span>
+                <span>&#8358;{{$hist->amount}}</span>
             </div>
             <div class="w-list2">
-                <p>D-T= 2023-08-26 2-45-09</p>
-                <span>Paid</span>
+                <p>{{$hist->created_at}}</p>
+                <span>{{$hist->status}}</span>
             </div>
         </div>
     </div>
-    <div class="withd">
-        <div class="withd-list">
-            <div class="w-list1">
-                <p>Flash-Grow</p>
-                <span>&#8358;12,000</span>
-            </div>
-            <div class="w-list2">
-                <p>D-T= 2023-08-2 2-45-09</p>
-                <span>Paid</span>
-            </div>
-        </div>
-    </div>
-    <div class="withd">
-        <div class="withd-list">
-            <div class="w-list1">
-                <p>Flash-Grow</p>
-                <span>&#8358;7,000</span>
-            </div>
-            <div class="w-list2">
-                <p>D-T= 2023-02-26 2-45-09</p>
-                <span>Paid</span>
-            </div>
-        </div>
-    </div>
+    @endforeach
+    @else
+
+    @endif
+
 </body>
 </html>
